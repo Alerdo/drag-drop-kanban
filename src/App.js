0c0fe24f051429cd99088e0bbfef0d0c84221cf2
Alerdo <alerdo23@gmail.com>
2023-08-24 21:19:06 +0100

    First Comiit, add content App.js , App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const DraggableItem = ({ item, index, onDragStart }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="draggable-item"
+      draggable
+      onDragStart={(e) => onDragStart(e, index)}
+    >
+      {item}
     </div>
   );
-}
+};
+
+const DroppableZone = ({ items, setItems, handleDragStart }) => {
+  const handleDrop = (e, newIndex) => {
+    e.preventDefault();
+    const draggedIndex = Number(e.dataTransfer.getData('text'));
+    const newItems = [...items];
+    const draggedItem = newItems[draggedIndex];
+    newItems.splice(draggedIndex, 1);
+    newItems.splice(newIndex, 0, draggedItem);
+    setItems(newItems);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="droppable-zone">
+      {items.map((item, index) => (
+        <div
+          key={index}
+          onDrop={(e) => handleDrop(e, index)}
+          onDragOver={handleDragOver}
+        >
+          <DraggableItem
+            item={item}
+            index={index}
+            onDragStart={handleDragStart}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
+
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData('text', index);
+  };
+
+  return (
+    <div className="container">
+      <DroppableZone items={items} setItems={setItems} handleDragStart={handleDragStart} />
+    </div>
+  );
+};
 
 export default App;
